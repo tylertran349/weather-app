61d48cb8b8e61494f99c1be2ac9ac6cee2713b01
tylertran349 <tylertran349@gmail.com>
2022-12-31 18:19:53 -0800

    Ignore this message

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import { getLatAndLon, forecastData } from '../dist/modules/api';
+import { getLatAndLon } from '../dist/modules/api';
 
-getLatAndLon("Miami");
+getLatAndLon(79832);
+
+let searchButton = document.querySelector('#search-button');
+let searchBox = document.querySelector('#search-box');
+
+function printGreeting(event) {
+    event.preventDefault();
+    console.log(searchBox.value);
+}
+
+searchButton.addEventListener('click', printGreeting, false);
