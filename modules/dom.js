f52433cfe41d76df329925e644a606c26cf43f0e
tylertran349 <tylertran349@gmail.com>
2023-01-02 21:28:45 -0800

    First stable build

--- modules/dom.js
+++ modules/dom.js
@@ -1,21 +1,6 @@
+import { weatherAppObject } from '../../src/index';
+
 let content = document.querySelector('#content');
-
-let tempLabel, windLabel, distanceLabel, precipLabel;
-
-export function updateDisplayLabelUnits(units) {
-    if(units === "imperial") {
-        tempLabel = "°F";
-        windLabel = "mph";
-        precipLabel = "in";
-        distanceLabel = "mi"
-    }
-    if(units === "metric") {
-        tempLabel = "°C";
-        windLabel = "m/s";
-        precipLabel = "mm";
-        distanceLabel = "km"
-    }
-}
 
 export function displayCurrentWeather(obj) {
     // Clear any weather data that is currently on the screen before displaying new weather data
@@ -27,34 +12,119 @@ export function displayCurrentWeather(obj) {
     let icon = document.createElement('img');
     let description = document.createElement('span');
 
+    // Re-use these elements multiple times when creating feels like, wind, humidity, and visibility subsections
+    let subsection; 
+    let valueTitle;
+    let value;
+
     currentWeather.setAttribute('id', 'current-weather');
 
     // Current weather title
-    title.innerHTML = `<strong>Current Weather</strong> - ${obj.location}, ${obj.country}`
+    title.innerHTML = `<strong>Current Weather</strong> - ${obj.location}, ${obj.country}`;
     title.setAttribute('id', 'section-title');
     currentWeather.appendChild(title);
 
-    temp.textContent = `${Math.round(obj.temp)} ${getTempLabel()}`;
+    temp.textContent = `${Math.round(obj.temp)} ${weatherAppObject.tempLabel}`;
     temp.setAttribute('id', 'temp');
     currentWeather.appendChild(temp);
+
+    icon.setAttribute('id', 'icon');
+    icon.setAttribute('src', `./images/${interpretOpenWeatherIcon(obj.icon)}`);
+    icon.setAttribute('title', `${obj.description}`);
+    currentWeather.appendChild(icon);
+
+    description.textContent = (obj.description).charAt(0).toUpperCase() + (obj.description).slice(1); // slice(1) gets rest of description besides first character
+    description.setAttribute('id', 'description');
+    currentWeather.appendChild(description);
+
+    // Feels like subsection
+    subsection = document.createElement('div');
+    valueTitle = document.createElement('span');
+    value = document.createElement('span');
+    valueTitle.textContent = "Feels Like";
+    valueTitle.setAttribute('id', 'value-title');
+    subsection.appendChild(valueTitle);
+    value.textContent = `${Math.round(obj.feelsLike)} ${weatherAppObject.tempLabel}`;
+    value.setAttribute('id', 'value');
+    subsection.setAttribute('id', 'subsection');
+    subsection.appendChild(value);
+    currentWeather.appendChild(subsection);
+
+    // Wind subsection
+    subsection = document.createElement('div');
+    valueTitle = document.createElement('span');
+    value = document.createElement('span');
+    valueTitle.textContent = "Wind";
+    valueTitle.setAttribute('id', 'value-title');
+    subsection.appendChild(valueTitle);
+    value.textContent = `${Math.round(obj.wind)} ${weatherAppObject.windLabel}`;
+    value.setAttribute('id', 'value');
+    subsection.setAttribute('id', 'subsection');
+    subsection.appendChild(value);
+    currentWeather.appendChild(subsection);
+
+    // Humidity subsection
+    subsection = document.createElement('div');
+    valueTitle = document.createElement('span');
+    value = document.createElement('span');
+    valueTitle.textContent = "Humidity";
+    valueTitle.setAttribute('id', 'value-title');
+    subsection.appendChild(valueTitle);
+    value.textContent = `${obj.humidity}%`;
+    value.setAttribute('id', 'value');
+    subsection.setAttribute('id', 'subsection');
+    subsection.appendChild(value);
+    currentWeather.appendChild(subsection);
+
+    // Visibility subsection
+    subsection = document.createElement('div');
+    valueTitle = document.createElement('span');
+    value = document.createElement('span');
+    valueTitle.textContent = "Visibility";
+    valueTitle.setAttribute('id', 'value-title');
+    subsection.appendChild(valueTitle);
+    value.textContent = `${obj.visibility / 1000} ${weatherAppObject.distanceLabel}`;
+    value.setAttribute('id', 'value');
+    subsection.setAttribute('id', 'subsection');
+    subsection.appendChild(value);
+    currentWeather.appendChild(subsection);
 
     content.appendChild(currentWeather); 
 }
 
-function getTempLabel() {
-    return tempLabel;
-}
-
-function getWindLabel() {
-    return windLabel;
-}
-
-function getDistanceLabel() {
-    return distanceLabel;
+function interpretOpenWeatherIcon(iconCode) {
+    if(iconCode === "01d" || iconCode === "01n") {
+        return "sunny.svg";
+    } else if(iconCode === "02d" || iconCode === "02n") {
+        return "partly-cloudy.svg";
+    } else if(iconCode === "03d" || iconCode === "03n") {
+        return "mostly-cloudy.svg";
+    } else if(iconCode === "04d" || iconCode === "04n") {
+        return "cloudy.svg";
+    } else if(iconCode === "09d" || iconCode === "09n") {
+        return "showers.svg";
+    } else if(iconCode === "10d" || iconCode === "10n") {
+        return "rain.svg";
+    } else if(iconCode === "11d" || iconCode === "11n") {
+        return "thunderstorms.svg";
+    } else if(iconCode === "13d" || iconCode === "13n") {
+        return "snow.svg";
+    } else if(iconCode === "50d" || iconCode === "50n") {
+        return "mist.svg";
+    }
 }
 
 function clearWeatherDisplay() {
-    document.querySelectorAll('#current-weather').remove();
-    document.querySelectorAll('#hour-by-hour-weather').remove();
-    document.querySelectorAll('#seven-day-weather').remove();
+    let currentWeather = document.querySelector('#current-weather');
+    let hourByHourWeather = document.querySelector('#hour-by-hour-weather');
+    let sevenDayWeather = document.querySelector('#seven-day-weather');
+    if(currentWeather !== null) {
+        currentWeather.remove();
+    }
+    if(hourByHourWeather !== null) {
+        hourByHourWeather.remove();
+    }
+    if(sevenDayWeather !== null) {
+        sevenDayWeather.remove();
+    }
 }
