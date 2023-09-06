4ad95d25ec86b6aeebfd371116a5b46ea56c1100
tylertran349 <tylertran349@gmail.com>
2023-09-06 15:18:11 -0700

    Fix Webpack issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
-const path = require('path');
+const path = require(`path`);
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: `./src/index.js`,
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: `main.js`,
+    path: path.resolve(__dirname, `dist`),
+  },
+  resolve: {
+    fallback: {
+      "fs": false,
+      "crypto": false,
+      "process": require.resolve("process/browser")
+    }
   },
 };
